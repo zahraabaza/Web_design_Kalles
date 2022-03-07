cf6ca6c2029cc42a30e500a857899fe80c627c34
zahraabaza <72666176+zahraabaza@users.noreply.github.com>
2022-03-08 00:16:04 +0200

    Add files via upload

---
+++ kalles.js
@@ -0,0 +1,35 @@
+let analogue = document.getElementById("analogue");
+analogue.addEventListener("mouseover", function(){
+    analogue.src = "./img/analogue2.jpg"
+})
+analogue.addEventListener("mouseout", function(){
+    analogue.src = "./img/analogue.jfif"
+})
+let blush = document.getElementById("blush");
+blush.addEventListener("mouseover", function(){
+    blush.src = "./img/blush2.jpg"
+})
+blush.addEventListener("mouseout", function(){
+    blush.src = "./img/blush.jpg"
+})
+let ridly = document.getElementById("ridly");
+ridly.addEventListener("mouseover", function(){
+    ridly.src = "./img/ridly2.jpg"
+})
+blush.addEventListener("mouseout", function(){
+    ridly.src = "./img/ridly.jpg"
+})
+let tshirt = document.getElementById("t-shirt");
+tshirt.addEventListener("mouseover", function(){
+    tshirt.src = "./img/t-shirt2.jpg"
+})
+tshirt.addEventListener("mouseout", function(){
+    tshirt.src = "./img/t-shirt.jpg"
+})
+let cwatch = document.getElementById("cwatch");
+cwatch.addEventListener("mouseover", function(){
+    cwatch.src = "./img/watch2.jfif"
+})
+cwatch.addEventListener("mouseout", function(){
+    cwatch.src = "./img/watch.png"
+})
